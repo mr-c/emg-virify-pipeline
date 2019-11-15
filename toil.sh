8db58d418e3d1190110f491fecc59e65305a87da
Martin Hoelzer <mhoelzer@noah-login-02.ebi.ac.uk>
2019-11-15 15:33:59 +0000

    wip toil exec

--- toil.sh
+++ toil.sh
@@ -8,16 +8,30 @@
 #export CONDA_ENV=/hps/nobackup2/production/metagenomics/pipeline/tools-v5/miniconda2-4.6.14/bin/activate
 # activate TOIL env
 source /hps/nobackup2/production/metagenomics/pipeline/tools-v5/toil-user-env/bin/activate
+
+## SINGULARITY GENERAL
+DIR=/hps/nobackup2/singularity/mhoelzer/build
+export SINGULARITY_CACHEDIR=$DIR/.singularity
+export SINGULARITY_TMPDIR=$DIR/.singularity/tmp
+export SINGULARITY_LOCALCACHEDIR=$DIR/singularity/tmp
+export SINGULARITY_PULLFOLDER=$DIR/.singularity/pull
+export SINGULARITY_BINDPATH=$DIR/.singularity/scratch
+
+## CWL SINGULARITY
+export CWL_SINGULARITY_CACHE=/hps/nobackup2/singularity/mhoelzer/
+
+### TOIL stuff
 export WORK_DIR=/homes/mhoelzer/data/toil/work
 export OUT_DIR=/homes/mhoelzer/data/toil/out
 export MEMORY=20G
-export NAME_RUN=test
+export NAME_RUN=virify
 export CWL=/homes/mhoelzer/backuped/git/CWL_viral_pipeline/CWL/WorkFlow/pipeline.cwl
 export YML=/homes/mhoelzer/backuped/git/CWL_viral_pipeline/CWL/WorkFlow/pipeline.yml
 export JOB_TOIL_FOLDER=$WORK_DIR/$NAME_RUN/
 export LOG_DIR=${OUT_DIR}/logs_${NAME_RUN}
 export TMPDIR=${WORK_DIR}/global-temp-dir_${NAME_RUN}
 export OUT_TOOL=${OUT_DIR}/${NAME_RUN}
+
 ###  RUN
 echo "run first part"
 mkdir -p $JOB_TOIL_FOLDER $LOG_DIR $TMPDIR $OUT_TOOL && \
@@ -35,4 +49,4 @@ time toil-cwl-runner \
   --writeLogs $LOG_DIR \
   --retryCount 0  \
   --logFile $LOG_DIR/${NAME_RUN}.log \
-$CWL_1 $YML 
+$CWL $YML 
