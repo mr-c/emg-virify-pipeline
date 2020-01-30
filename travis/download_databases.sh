222b6c73f222cb306bcce5f2e68af374a5c6ec36
Martin Beracochea <mbc@ebi.ac.uk>
2020-01-30 16:17:07 +0000

    wget block scrool - travis output size

--- travis/download_databases.sh
+++ travis/download_databases.sh
@@ -8,7 +8,7 @@ then
     mkdir -p "$1/virsorter-data" && \
     wget \
         ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/VirSorter_data_viral/virsorter-data-v2.tar.gz -O virsorter-data-v2.tar.gz \
-        --show-progress && \
+        --progress=bar:force:noscroll && \
     tar --extract --gzip --verbose --file=virsorter-data-v2.tar.gz --directory="$1" && \
     rm virsorter-data-v2.tar.gz
 else
@@ -19,7 +19,8 @@ if [ ! -d "$1/vpHMM" ];
 then
   mkdir -p "$1/vpHMM" && \
   wget \
-    ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/vpHMM_database.tar.gz -O vpHMM_database.tar.gz --show-progress && \
+    ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/vpHMM_database.tar.gz -O vpHMM_database.tar.gz \
+    --progress=bar:force:noscroll && \
   tar --extract --gzip --verbose --file=vpHMM_database.tar.gz --directory="$1/vpHMM" --strip-components=1 && \
   rm vpHMM_database.tar.gz
 else
@@ -30,7 +31,7 @@ if [ ! -f "$1/ete3_ncbi_tax.sqlite" ];
 then
   wget \
     ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/viral-pipeline/ete3_ncbi_tax.sqlite.gz -O ete3_ncbi_tax.sqlite.gz \
-    --show-progress && \
+    --progress=bar:force:noscroll && \
   gunzip -c ete3_ncbi_taxX.sqlite.gz > "$1/ete3_ncbi_tax.sqlite"
 else
   echo '# ete3_ncbi_tax present in cache'
@@ -40,7 +41,7 @@ if [ ! -f "$1/vphmm_2020-01-29.pickle" ];
 then
   wget \
     ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/viral-pipeline/vphmm_2020-01-29.pickle -O "$1/vphmm_2020-01-29.pickle" \
-    --show-progress
+    --progress=bar:force:noscroll
 else
   echo '# vphmm_2020-01-29.pickle present in cache'
 fi
